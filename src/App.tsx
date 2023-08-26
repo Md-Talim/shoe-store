90a6af17c80aa32b63d4ea67b7f7afb44a9359d6
Mohammad Talim <talimmohammad116@gmail.com>
2023-08-26 18:03:52 +0530

    Build the reviews section

--- src/App.tsx
+++ src/App.tsx
@@ -29,7 +29,6 @@ const App = () => (
       <SpecialOffer />
     </section>
     <section className="bg-pale-blue padding">
-      {' '}
       <CustomerReviews />
     </section>
     <section className="padding-x sm:py-32 py-16 w-full">
--- src/components/index.ts
+++ src/components/index.ts
@@ -1,4 +1,6 @@
 export { default as Button } from './Button';
 export { default as Navbar } from './Navbar';
+export { default as PopularProductCard } from './PopularProductCard';
+export { default as ReviewCard } from './ReviewCard';
 export { default as ServiceCard } from './ServiceCard';
 export { default as ShoeCard } from './ShoeCard';
