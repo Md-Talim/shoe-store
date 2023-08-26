7fe784fb5347d7a6419e4f61eeaf594d13b3b2b0
Mohammad Talim <talimmohammad116@gmail.com>
2023-08-26 17:41:58 +0530

    Build the Super Quality section

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,17 +1,19 @@
 interface Props {
   label: string;
-  imageUrl: string;
+  imageUrl?: string;
 }
 
 const Button = ({ label, imageUrl }: Props) => {
   return (
     <button className="flex items-center justify-center gap-4 px-8 py-4 bg-coral-red border border-coral-red text-white rounded-full font-montserrat">
       {label}
-      <img
-        src={imageUrl}
-        alt="Right Arrow Icon"
-        className="rounded-full h-5 w-5"
-      />
+      {imageUrl && (
+        <img
+          src={imageUrl}
+          alt="Right Arrow Icon"
+          className="rounded-full h-5 w-5"
+        />
+      )}
     </button>
   );
 };
