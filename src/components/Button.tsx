8839bc8a455240f7f0f0118a6ee1306eb0d7f3d3
Mohammad Talim <talimmohammad116@gmail.com>
2023-08-26 17:51:20 +0530

    Build the Special Offer section

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,11 +1,26 @@
 interface Props {
   label: string;
   imageUrl?: string;
+  borderColor?: string;
+  backgroundColor?: string;
+  textColor?: string;
 }
 
-const Button = ({ label, imageUrl }: Props) => {
+const Button = ({
+  label,
+  imageUrl,
+  backgroundColor,
+  borderColor,
+  textColor,
+}: Props) => {
   return (
-    <button className="flex items-center justify-center gap-4 px-8 py-4 bg-coral-red border border-coral-red text-white rounded-full font-montserrat">
+    <button
+      className={`flex items-center justify-center gap-4 px-8 py-4 border ${
+        backgroundColor
+          ? `${backgroundColor} ${textColor} ${borderColor}`
+          : 'bg-coral-red border-coral-red text-white'
+      } rounded-full font-montserrat`}
+    >
       {label}
       {imageUrl && (
         <img
