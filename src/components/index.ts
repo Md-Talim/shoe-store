b4b5cbf0d04098fe539fa19ab4e064a83ba61cd8
Mohammad Talim <talimmohammad116@gmail.com>
2023-08-26 17:42:08 +0530

    Build the Services Section

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,3 +1,4 @@
 export { default as Button } from './Button';
 export { default as Navbar } from './Navbar';
+export { default as ServiceCard } from './ServiceCard';
 export { default as ShoeCard } from './ShoeCard';
